b4955d3f0985ce16d07b6a138d71413bdd2bbfb3
ChoeruNisaAmalia <72701440+ChoeruNisaAmalia@users.noreply.github.com>
2020-10-22 19:07:24 +0700

    Update auth_header.php

--- application/views/Administrator/templates/auth_header.php
+++ application/views/Administrator/templates/auth_header.php
@@ -11,16 +11,16 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <link rel="icon" type="image/png" href="<?= base_url() ?>assets/img/icon-lambang-kecamatan.png">
-  <title><?= $title ?></title>
+  <link rel="icon" type="image/png" href="<?= filter_var(base_url())?>assets/img/icon-lambang-kecamatan.png">
+  <title><?= filter_var($title) ?></title>
 
   <!-- Custom fonts for this template-->
-  <link href="<?= base_url()?>assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="<?= filter_var(base_url())?>assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="<?= base_url()?>assets/css/sb-admin-2.min.css" rel="stylesheet">
-  <link href="<?= base_url()?>assets/css/sb-admin-2.css" rel="stylesheet">
+  <link href="<?= filter_var(base_url())?>assets/css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="<?= filter_var(base_url())?>assets/css/sb-admin-2.css" rel="stylesheet">
 
 </head>
 

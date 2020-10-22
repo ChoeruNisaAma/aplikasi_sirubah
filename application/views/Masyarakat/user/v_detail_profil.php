0805322934d2a36dbc526b5772af4fee760f1556
ChoeruNisaAmalia <72701440+ChoeruNisaAmalia@users.noreply.github.com>
2020-10-22 16:30:33 +0700

    Update v_detail_profil.php

--- application/views/Masyarakat/user/v_detail_profil.php
+++ application/views/Masyarakat/user/v_detail_profil.php
@@ -8,7 +8,7 @@
     
     <div class="row">
         <div class="col">
-            <?= $this->session->flashdata('message'); ?>
+            <?= filter_var($this->session->flashdata('message')); ?>
         </div>
     </div>
     
@@ -16,11 +16,11 @@
 		<div class="row">
 	    	<div class="col-4" style="text-align: center;">
 	    		<div style="margin-bottom: 15px;">
-	        		<img class="img-profile rounded-circle" src="<?= base_url('assets/img/profil/').$user['profil']?>" alt="Foto" style="height: 250px; width: 250px;">
+	        		<img class="img-profile rounded-circle" src="<?= filter_var(base_url('assets/img/profil/').$user['profil']);?>" alt="Foto" style="height: 250px; width: 250px;">
 	    		</div>
 				
-				<strong style="font-size: 30px"><?= $user['nama'];?></strong>		
-				<div style="font-size: 20px">NIK. <?= $user['nik']; ?></div>
+				<strong style="font-size: 30px"><?= filter_var($user['nama']);?></strong>		
+				<div style="font-size: 20px">NIK. <?= filter_var($user['nik']); ?></div>
 	    	</div>
 
 	    	<div class="col-7 mb-2">
@@ -29,52 +29,52 @@
 						<div class="col-sm-2" style="font-size: 18px">
 							<b>Kelurahan</b>
 						</div>
-						<div class="col-sm-9" style="font-size: 18px"><?= $user['nama_pengurus']; ?></div>
+						<div class="col-sm-9" style="font-size: 18px"><?= filter_var($user['nama_pengurus']); ?></div>
 					</div>
 				
 					<div class="row web_rowdetail mb-1">
 						<div class="col-sm-2" style="font-size: 18px ">
 							<b>Nama RT</b>
 						</div>
-						<div class="col-sm-9" style="font-size: 18px">RT <?= $user['nama_rt']; ?></div>
+						<div class="col-sm-9" style="font-size: 18px">RT <?= filter_var($user['nama_rt']); ?></div>
 					</div>
 
 					<div class="row web_rowdetail mb-1">
 						<div class="col-sm-2" style="font-size: 18px">
 							<b>Nama RW</b>
 						</div>
-						<div class="col-sm-9" style="font-size: 18px">RW <?= $user['nama_rw']; ?></div>
+						<div class="col-sm-9" style="font-size: 18px">RW <?= filter_var($user['nama_rw']); ?></div>
 					</div>
 
 					<div class="row web_rowdetail mb-1">
 						<div class="col-sm-2" style="font-size: 18px">
 							<b>Email</b>
 						</div>
-						<div class="col-sm-9" style="font-size: 18px"><?= $user['email_msy']; ?></div>
+						<div class="col-sm-9" style="font-size: 18px"><?= filter_var($user['email_msy']); ?></div>
 					</div>
 
 					<div class="row web_rowdetail mb-1">
 						<div class="col-sm-2" style="font-size: 18px">
 							<b>No HP</b>
 						</div>
-						<div class="col-sm-9" style="font-size: 18px"><?= $user['no_hp']; ?></div>
+						<div class="col-sm-9" style="font-size: 18px"><?= filter_var($user['no_hp']); ?></div>
 					</div>
 
 					<div class="row web_rowdetail mb-1">
 						<div class="col-sm-2" style="font-size: 18px">
 							<b>Alamat</b>
 						</div>
-						<div class="col-sm-9" style="font-size: 18px"><?= $user['alamat']; ?></div>
+						<div class="col-sm-9" style="font-size: 18px"><?= filter_var($user['alamat']); ?></div>
 					</div>
 				</div>
 
 				<div class="my-3">
-					<img class="m-auto" src="<?= base_url('assets/img/profil/').$user['foto_ktp']?>" style="width: 450px"><br>
+					<img class="m-auto" src="<?= filter_var(base_url('assets/img/profil/').$user['foto_ktp']);?>" style="width: 450px"><br>
 				</div>
 
 				<div>
-		        	<a href="<?= base_url('profil_masyarakat/edit')?>" class="btn btn-primary"><i class="fas fa-edit mx-1"></i>Edit</a>
-		        	<a class="btn btn-danger text-white" onClick="deleteConfirm('<?= base_url('profil_masyarakat/hapus_akun/'.$user['nik'])?>')" data-toggle="modal" data-target="#hapusModal"><i class="fas fa-trash-alt mx-1"></i>Hapus Akun</a>
+		        	<a href="<?= filter_var(base_url('profil_masyarakat/edit'));?>" class="btn btn-primary"><i class="fas fa-edit mx-1"></i>Edit</a>
+		        	<a class="btn btn-danger text-white" onClick="deleteConfirm('<?= filter_var(base_url('profil_masyarakat/hapus_akun/'.$user['nik']));?>')" data-toggle="modal" data-target="#hapusModal"><i class="fas fa-trash-alt mx-1"></i>Hapus Akun</a>
 		    	</div>							
 	   		</div>
 		</div>
